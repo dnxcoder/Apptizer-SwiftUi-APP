6cda0046b64a64a62f754760a8433e750052ada0
Denis Coder <dnxcoder@gmail.com>
2024-02-17 22:30:25 -0600

    used networkManager to feed UI List

--- Apptizers/AppetizerListView.swift
+++ Apptizers/AppetizerListView.swift
@@ -8,12 +8,39 @@
 import SwiftUI
 
 struct AppetizerListView: View {
+    
+    @State private var appetizers: [Appetizer] = []
+    
+    
+    
+    
     var body: some View {
         NavigationView {
-            List(MockData.appetizers, id:\.id) { appetizer in
+            List(appetizers, id:\.id) { appetizer in
                 AppetizerListCell(appetizer: appetizer)
             }
             .navigationTitle("🍟 Appetizers")
+        }.onAppear{
+            getApptizers()
+        }
+    }
+    
+    func getApptizers() {
+        NetworkManager.shared.getAppetizers(){ result in
+            
+            //DispatchQueue.main.async makes the code run in the main thread
+            // since we have the NetworkManager that would work on the background thread
+            // but since we are going to use the results from NetworkManager to change our UI
+            // we need to use DispatchQueue.main.async
+            
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let appetizers):
+                    self.appetizers = appetizers
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
         }
     }
 }
